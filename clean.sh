39596948715e2766a6738b19212bf4cb8e07c531
Muthukumaran Chandrasekaran <mkran1985@gmail.com>
2017-10-13 19:49:02 -0400

    adding template for iArduSubComms

--- clean.sh
+++ clean.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-rm -rf build/*
+rm -rf build/C*
+rm -rf build/s*
+rm -rf build/c*
+rm -rf build/M*
+
 rm -rf lib/*
 rm -rf bin/p*
+rm -rf bin/i*
 
 find . -name '.DS_Store'  -print -exec rm -rfv {} \;
 find . -name '*~'  -print -exec rm -rfv {} \;
